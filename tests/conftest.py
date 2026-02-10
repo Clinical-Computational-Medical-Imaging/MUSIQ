292da098dfce4196969fb19f9bafb9cdd8a8520f
boehrijo <jonas.boehringer@uni-a.de>
2026-02-10 17:12:01 +0100

    add files for test and global fixtures, correct pyproject.toml for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.fixture(scope="session")
+def input_dirpath():
+        path = 
+        yield path
+
+def output_dirpath():
+        path = 
+        yield path
+
+def series_keywords():
+        dict = []
+        yield dict
---
+++ tests/test_series_selection_json.py
@@ -0,0 +1,6 @@
+import json
+import runpy
+from pathlib import Path
+
+def test_collect_series(input_dirpath, output_dirpath, series_keywords):
+    
